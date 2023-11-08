dee2d7d584869f0fbac53261e049b537b20d203c
Peter <pedropseverino@gmail.com>
2023-11-08 09:37:13 -0400

    my second commit

---
+++ Hello_world.py
@@ -0,0 +1,11 @@
+Python 3.11.1 (v3.11.1:a7a450f84a, Dec  6 2022, 15:24:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> print("hello World")
+hello World
+
+Age = 15
+if age == 15:
+	print("Age is equal to 15")
+else:
+	print("Age is equal to 15")
